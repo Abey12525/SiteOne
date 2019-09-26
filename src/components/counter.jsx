c9d3ef70755cac4ad95e6553a96aa51ae419cf3c
Abey12525 <abeyrhurtis@cs.ajce.in>
2019-09-27 03:03:27 +0530

    build_0.2

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+
 class Counter extends Component {
    
     state = {
-        count : 0
+        value : this.props.counter.value
     };
 
     // used to replace arrow function
@@ -18,21 +19,21 @@ class Counter extends Component {
 
     classExtract() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
     
     functionTest = () => {
-        this.setState(({count}) => ({
-            count : count + 1
+        this.setState(({value}) => ({
+            value : value + 1
         }));
     };
-
     render() { 
         return (
             <React.Fragment>
-                <span style={this.styles} className = {this.classExtract()}>{this.state.count}</span>
+                <span style={this.styles} className = {this.classExtract()}>{this.state.value}</span>
                 <button className = "btn btn-secondary btn-sm" onClick = {this.functionTest}>Click Here</button>
+                <button className = "btn btn-danger btn-sm" onClick = {() => this.props.onDelete(this.props.counter.id)}>delete</button>
             </React.Fragment>
         );
     };
--- src/components/multiCount.jsx
+++ src/components/multiCount.jsx
@@ -3,11 +3,25 @@ import Counter from './counter';
 
 class MultiCount extends Component{
 
+    state = {
+        counters: [
+            {id:1, value:0},
+            {id:2, value:1}
+        ]
+    };
+
+    handleDelete = (counterId) => {
+        console.log("Delete Event handel",counterId);
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({counters})
+    };
+
     render(){
         return (
             <React.Fragment>
-            <Counter/>
-            <Counter/>
+                {this.state.counters.map(counter => (
+                    <Counter key={counter.id} onDelete={this.handleDelete} counter={counter} />
+                ))}
             </React.Fragment>
         );
     }
