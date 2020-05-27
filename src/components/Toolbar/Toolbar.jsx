6f15d4174ee0fe32eaf1d6aa2bda1538f2b655c6
Abey12525 <abeyrhurtis@cs.ajce.in>
2020-05-27 13:37:57 +0530

    design_update3

--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -13,9 +13,9 @@ const Toolbar = props => (
             <div className="spacer"/>
             <div className="toolbar__navigation-items">
                 <ul>
-                    <li><a href="/">Projects</a></li>
-                    <li><a href="/">Blog</a></li>
-                    <li><a href="/">Dev</a></li>
+                    <li><a href="/">PROJECTS</a></li>
+                    <li><a href="/">BLOG</a></li>
+                    <li><a href="/">DEV</a></li>
 
 
                 </ul>
